d2d3fb7b427efbf8e3c20782682ffe8d25ce2b46
Mirko Caserta <mirko.caserta@gmail.com>
2025-09-03 11:47:26 -0600

    version string interpolation in urls

--- Formula/krik.rb
+++ Formula/krik.rb
@@ -6,17 +6,17 @@ class Krik < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mcaserta/krik/releases/download/v0.1.27/kk-macos-aarch64"
+      url "https://github.com/mcaserta/krik/releases/download/v#{version}/kk-macos-aarch64"
       sha256 "fa113cecc62d39f33140b0a6cccbcbb20e323b1c3a9b356bae7b6d57fd877ab1"
     else
-      url "https://github.com/mcaserta/krik/releases/download/v0.1.27/kk-macos-x86_64"
+      url "https://github.com/mcaserta/krik/releases/download/v#{version}/kk-macos-x86_64"
       sha256 "3e831276ca793f5a782ae428b1a18ac4359468446ef3c996f94fa4a4a403ebfd"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/mcaserta/krik/releases/download/v0.1.27/kk-linux-x86_64"
+      url "https://github.com/mcaserta/krik/releases/download/v#{version}/kk-linux-x86_64"
       sha256 "0fe7af8e94163adccb7b49a746fc374175ee509c9931c14c229d62215bedfa69"
     end
   end
